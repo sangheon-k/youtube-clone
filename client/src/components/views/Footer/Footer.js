f5cc21f0555f6ee9ce734fd30ed1a512f41cb52b
sangheon-k <sangheon.k@kakao.com>
2023-03-19 21:50:43 +0900

    add : backup

---
+++ client/src/components/views/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {Icon} from 'antd';
+import './Footer.css'
+
+function Footer() {
+    return (
+        <footer>
+           <p> Copyright By <Icon type="smile" /> Sangheon </p>
+        </footer>
+    )
+}
+
+export default Footer
